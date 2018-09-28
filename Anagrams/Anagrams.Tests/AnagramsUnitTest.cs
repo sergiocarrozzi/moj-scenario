7eb81dd45131745d8b278e0685236ce9516abd23
Sergio <sergio.carrozzi@gmail.com>
2018-09-28 12:36:45 +0200

    Anagram: added first versione of IsAnagramsAlgorithmWorksCorrectly test

---
+++ Anagrams/Anagrams.Tests/AnagramsUnitTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.IO;
+
+namespace Anagrams.Tests
+{
+    [TestClass]
+    public class AnagramsUnitTest
+    {
+        /// <summary>
+        /// url for requesting an anagram
+        /// </summary>
+        private string url = "http://localhost:51906/Anagram";
+
+        [TestMethod]
+        public void IsAnagramsAlgorithmWorksCorrectly()
+        {
+            string urlWithParameters = $"{url}?words=crepitus";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            Stream resStream = response.GetResponseStream();
+            
+        }
+    }
+}
